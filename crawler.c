5685bf0b5dc70572ca3c375f5a775729e700e5b6
sk2518 <98252531+sk2518@users.noreply.github.com>
2023-04-27 13:03:47 -0400

    Add files via upload

--- crawler.c
+++ crawler.c
@@ -1,18 +1,44 @@
+/*
+Michael Helf
+Ahtsham Sohail
+Shahpur Khan
+
+50:198:341 Operating Systems - Spring 2023
+Group Project: Multithreaded Web Crawler
+
+
+*/
+
+// Set our caps for request count, link count, etc.
 int max_con = 200;
 int max_total = 200;
 int max_requests = 50;
 int max_link_per_page = 5;
 int follow_relative_links = 0;
 
+// Libxml is a C library for data manipulation.
 
+// Relevant to this, we will include its HTML parser,
 #include <libxml/HTMLparser.h>
+// the XPath language for doc addressing purposes,
 #include <libxml/xpath.h>
+// and the library of generic Resource Identification routines.
 #include <libxml/uri.h>
+// We will also include...
+// curl, a URL-focused data transferring tool,
 #include <curl/curl.h>
+// Standard library for utility functions
+
 #include <stdlib.h>
+// String library for string manipulation etc
+
 #include <string.h>
+// Math library to use mathematical functions
+
 #include <math.h>
+
 #include <pthread.h>
+// And include signal.h (specifies signal-handling during execution)
 #include <signal.h>
 
 // Set a cap on URL length to minimize impacts to performance
@@ -22,9 +48,13 @@ int follow_relative_links = 0;
 #define NORMAL "\x1b[m"
 #define GREEN "\x1b[32m"
 
+// Initialize pending interrupt with value zero
 int pending_interrupt = 0;
+// Sighandler should have no return value
+
 void sighandler(int dummy)
-{
+{    // Now, set it to 1
+
   pending_interrupt = 1;
 }
 
@@ -57,8 +87,10 @@ size_t grow_buffer(void *contents, size_t sz, size_t nmemb, void *ctx)
   return realsize;
 }
 
+// Make handle with URL arg
 CURL *make_handle(char *url)
-{
+{    // curl_easy_init returns an easy handle (to be inputted to other easy functions)
+
   CURL *handle = curl_easy_init();
 
   /* Important: use HTTP2 over HTTPS */
@@ -118,11 +150,15 @@ CURL *make_handle(char *url)
   curl_easy_setopt(handle, CURLOPT_EXPECT_100_TIMEOUT_MS, 0L);
 
 
+  // Finally return the handle
 
   return handle;
 }
 
 /* HREF finder implemented in libxml2 but could be any HTML parser */
+
+// Takes new links grabbed from the site and follows them
+// to ensure link validity
 size_t follow_links(CURLM *multi_handle, memory *mem, char *url)
 {
   int opts = HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | \
@@ -130,10 +166,14 @@ size_t follow_links(CURLM *multi_handle, memory *mem, char *url)
   htmlDocPtr doc = htmlReadMemory(mem->buf, mem->size, url, NULL, opts);
   if(!doc)
     return 0;
+    // Sets pretext to follow all "href" links in doc
   xmlChar *xpath = (xmlChar*) "//a/@href";
+  // Sets context to HTML document
   xmlXPathContextPtr context = xmlXPathNewContext(doc);
+  // Assigns all hrefs found in context to the variable result
   xmlXPathObjectPtr result = xmlXPathEvalExpression(xpath, context);
   xmlXPathFreeContext(context);
+  // Return 0 if no results found
   if(!result)
     return 0;
   xmlNodeSetPtr nodeset = result->nodesetval;
@@ -143,6 +183,10 @@ size_t follow_links(CURLM *multi_handle, memory *mem, char *url)
   }
   size_t count = 0;
   int i;
+
+  // Randomizes list of which links to follow
+  // NOT first come first serve
+  // In an effort to avoid efficiency losses
   for(i = 0; i < nodeset->nodeNr; i++) {
     double r = rand();
     int x = r * nodeset->nodeNr / RAND_MAX;
@@ -154,10 +198,15 @@ size_t follow_links(CURLM *multi_handle, memory *mem, char *url)
       xmlFree(orig);
     }
     char *link = (char *) href;
+
+    // Ensure that each href link receives begins with
+    // properly either "http://" or "https://".
     if(!link || strlen(link) < 20)
       continue;
     if(!strncmp(link, "http://", 7) || !strncmp(link, "https://", 8)) {
       curl_multi_add_handle(multi_handle, make_handle(link));
+
+      // Stops after we receive the Threshold of URLs per page.
       if(count++ == max_link_per_page)
         break;
     }
@@ -167,6 +216,7 @@ size_t follow_links(CURLM *multi_handle, memory *mem, char *url)
   return count;
 }
 
+// Returns True or False depending on validity of text
 int is_html(char *ctype)
 {
   return ctype != NULL && strlen(ctype) > 10 && strstr(ctype, "text/html");
